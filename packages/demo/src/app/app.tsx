90f6ee4c25bab81afc09991eb59e50f1aa7ca1bd
Vsevolod Zolotov <vsezold@gmail.com>
2024-07-13 00:10:21 +0300

    feat: redesign main page

--- packages/demo/src/app/app.tsx
+++ packages/demo/src/app/app.tsx
@@ -11,7 +11,6 @@ import {
   Page,
   PageContent,
   PageHeader,
-  Paragraph,
   Text,
   WorldMap,
   grommet,
@@ -19,6 +18,8 @@ import {
 import { Moon, SearchAdvanced, Sun } from 'grommet-icons';
 import { deepMerge } from 'grommet/utils';
 import { PropsWithChildren, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { Bug } from './bug';
 
 const theme = deepMerge(grommet, {
   global: {
@@ -44,18 +45,18 @@ const WorldMapTemplate = () => {
   return (
     <WorldMap
       fill={'vertical'}
-      color="graph-0"
+      color="dark-4"
       continents={[
         {
           name: 'Europe',
-          color: 'graph-1',
+          color: 'brand',
         },
       ]}
       places={[
         {
           name: 'Moscow',
           location: [55.44, 37.36],
-          color: 'graph-2',
+          color: 'status-critical',
         },
       ]}
       selectColor="brand"
@@ -63,11 +64,39 @@ const WorldMapTemplate = () => {
   );
 };
 
+const rotateRight = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const rotateLeft = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(-360deg);
+  }
+`;
+
+const Virus = styled.img<{ left?: boolean }>`
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  animation: ${({ left = false }) => (left ? rotateLeft : rotateRight)} 2s
+    linear infinite;
+`;
+
 const CardTemplate = ({ title, children }: CardProps) => {
   return (
     <Card elevation="none" border pad="medium">
       <CardHeader>
-        <Heading level={2} margin="none">
+        <Heading level={3} margin="none">
           {title}
         </Heading>
       </CardHeader>
@@ -77,13 +106,13 @@ const CardTemplate = ({ title, children }: CardProps) => {
 };
 
 export function App() {
-  const [dark, setDark] = useState(true);
+  const [dark, setDark] = useState(false);
 
   return (
     <Grommet theme={theme} themeMode={dark ? 'dark' : 'light'} full>
       <Page>
         <AppBar>
-          <Text size="large">@vsezol/lupa</Text>
+          <Text size="large">ПУПА ЛУПА</Text>
 
           <Button
             a11yTitle={dark ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
@@ -105,37 +134,71 @@ export function App() {
         </AppBar>
 
         <PageContent>
-          <PageHeader title="Welcome to @vsezol/lupa!" />
+          <PageHeader
+            alignSelf="center"
+            size="small"
+            title="Приветствуем всех Пуп и Луп!"
+          />
+
+          <Grid columns="medium" gap="small" pad={{ bottom: 'large' }}>
+            <CardTemplate title="Не видите баг? — Приблизьте!">
+              <Box direction="row" justify="center" pad={{ top: 'medium' }}>
+                <Box
+                  direction="row"
+                  justify="center"
+                  style={{ position: 'relative' }}
+                >
+                  <Bug
+                    style={{
+                      position: 'absolute',
+                      top: '50%',
+                      right: 4,
+                      transform: 'translate(-50%, -50%)',
+                    }}
+                  />
+                  <Button primary size="large" label="Не жмякается"></Button>
+                </Box>
+              </Box>
+            </CardTemplate>
+
+            <CardTemplate title="Вирусы на сайте? — Не проблема!">
+              <Box
+                direction="row"
+                justify="center"
+                pad={{ top: 'medium', bottom: 'medium' }}
+              >
+                <div style={{ position: 'relative', minHeight: '20px' }}>
+                  <Virus
+                    left
+                    src="/virus-1.png"
+                    alt="virus 1"
+                    style={{ left: 0, top: 20 }}
+                  />
+                  <Virus
+                    src="/virus-3.png"
+                    alt="virus 2"
+                    style={{ left: 20, top: 10 }}
+                  />
+                  <Virus left src="/virus-2.png" alt="virus 3" />
+                </div>
+              </Box>
+            </CardTemplate>
+
+            <CardTemplate title="Забудьте про ctrl + — есть лупа">
+              <Box direction="row" justify="center" pad={{ top: 'medium' }}>
+                <Button
+                  size="large"
+                  secondary
+                  icon={<SearchAdvanced size="medium" />}
+                  label="пупа-лупа!"
+                />
+              </Box>
+            </CardTemplate>
+          </Grid>
 
           <Box direction="row" justify="center">
             <WorldMapTemplate></WorldMapTemplate>
           </Box>
-
-          <Box direction="row" justify="center" pad="large">
-            <Box width="small">
-              <Button
-                fill={true}
-                size="large"
-                secondary
-                icon={<SearchAdvanced size="large" />}
-                label="Magnify"
-              />
-            </Box>
-          </Box>
-
-          <Grid columns="medium" gap="large" pad={{ bottom: 'large' }}>
-            <CardTemplate title="Framework agnostic">
-              <Paragraph>You can use it with any framework!</Paragraph>
-            </CardTemplate>
-
-            <CardTemplate title="Simple API">
-              <Paragraph>Just one class!</Paragraph>
-            </CardTemplate>
-
-            <CardTemplate title="Magnify everything">
-              <Paragraph>You can magnify any content on page!</Paragraph>
-            </CardTemplate>
-          </Grid>
         </PageContent>
       </Page>
     </Grommet>
